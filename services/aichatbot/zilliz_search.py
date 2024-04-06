6595ced88c98751aa646587cc7cf3db189cb6755
Yapeng Teng <tengyp99@gmail.com>
2024-04-06 10:30:44 -0400

    up

--- services/aichatbot/zilliz_search.py
+++ services/aichatbot/zilliz_search.py
@@ -151,11 +151,8 @@ def recommandGiftByUserInput(req):
     # Single vector search
     res = client.search(
         collection_name="products",
-        data=[[
-            0.3580376395471989, -0.6023495712049978, 0.18414012509913835,
-            -0.26286205330961354, 0.9029438446296592
-        ]],
-        limit=5,    # Max. number of search results to return
+        data=query_vector,
+        limit=10,    # Max. number of search results to return
         search_params={
             "metric_type": "COSINE",
             "params": {}
