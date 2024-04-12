ff7add0406bbc6a19826c925995f647606910b35
Yapeng Teng <tengyp99@gmail.com>
2024-04-12 01:00:54 -0400

    u

--- services/webhook/webhookService.py
+++ services/webhook/webhookService.py
@@ -4,7 +4,7 @@ from flask import current_app, g
 def webhookService(headers, json_data):
 
     topic = headers.get('X-Shopify-Topic')
-
+    current_app.logger.info(topic)
     current_app.logger.info(json_data)
 
     if topic == 'orders/create':
@@ -25,8 +25,51 @@ def webhookService(headers, json_data):
     elif topic == 'products/delete':
         return productDelete(req)
 
+    elif topic == 'customers/create':
+        return insertCustomerToMongoDB(json_data)
 
-def orderCreate(req):
-    data = req.get_json()
 
-    current_app.logger.info(data)
+def insertCustomerToMongoDB(data):
+
+    customer = customerCreate(data)
+    customerCollection(customer)
+
+    return 1
+
+
+def customerCollection(customer_data):
+    customer_collection = g.db['test2']["customers"]
+    customer_collection.insert_many(customer_data)
+
+    return 1
+
+
+def customerCreate(data):
+
+    address = data.get("adress", {})
+    customer = {
+        "customer_id": data.get("id", None),
+        "first_name": data.get("first_name", None),
+        "last_name": data.get("last_name", None),
+        "customer_email": data.get("email", None),
+        "accepts_email_marketing": data.get("email_marketing_consent", None),
+        "phone": data.get("phone", None),
+        "accepts_sms_marketing": data.get("sms_marketing_consent", None),
+        "total_spent": data.get("total_spent", 0.00),
+        "total_orders": data.get("orders_count", 0),
+        "note": data.get("note", None),
+        "tax_exempt": data.get("tax_exempt", "no"),
+        "tags": data.get("tags", None),
+        "default_address": {
+            "address1": data.get("addresses", None),
+            "address2": data.get("Default Address Address2", None),
+            "company": None,
+            "city": address.get("Default Address City", None),
+            "zip": address.get("Default Address Zip", None),
+            "province": address.get("Default Address Province Code", None),
+            "country": address.get("Default Address Country Code", None),
+            "phone": address.get("Default Address Phone", None),
+        },
+    }
+
+    return customer
