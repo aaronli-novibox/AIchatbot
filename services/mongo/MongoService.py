a75705cd1c7e28c6b53449547ae75067ad7c9190
Yamy-Z <ykz5402@psu.edu>
2024-04-24 17:07:43 -0400

    reset password and product list

--- services/mongo/MongoService.py
+++ services/mongo/MongoService.py
@@ -58,13 +58,13 @@ def insertInfluencerData(influencer_data):
     
 def getInflencerProductList(influencer_name):
     # Get Influencers Infor and products
-    influencers_collection = g.db['test2']["influencers"]
+    influencers_collection = g.db['test2']["new_influencers"]
     influencer_info = influencers_collection.find_one({'influencer_name': influencer_name} , {'product': 1, 'promo_code': 1})
     if influencer_info is None:
         print(f"No influncer found with name {influencer_name}")
         return 
     signed_products = influencer_info['product']
-    promo_code = influencer_info['promo_code']
+    #promo_code = influencer_info['promo_code']
 
     #Get all products
     products_collection = g.db['test2']["products"]
@@ -77,9 +77,9 @@ def getInflencerProductList(influencer_name):
         product['product_sku'] = None
         product['start_time'] = None
         product['end_time'] = None
-        product['total_products_purchased'] = 0
-        product['earnings_per_product'] = 0
-        product['total_commission'] = 0
+        #product['total_products_purchased'] = 0
+        #product['earnings_per_product'] = 0
+        #product['total_commission'] = 0
 
         #check if the product is signed with a commission rate
         is_signed = next((item for item in signed_products if item['product_name'] == product['title']), None)
@@ -91,29 +91,29 @@ def getInflencerProductList(influencer_name):
             product['end_time'] = is_signed['product_contract_end']
 
 
-    # Get orders with promo code
-    orders = g.db['test2']['new_filled_orders']
-    orders = orders.find({'promo_code': promo_code, 'financial_status': 'paid'}, {'lineitem': 1})
-    for order in orders:
-        #find corresponding product
-        product_name = order['lineitem'][0]['lineitem_name']
-        result = next((item for item in products if item['title'] == product_name), None)
-        price = order['lineitem'][0]['lineitem_price']
-        quantity = order['lineitem'][0]['lineitem_quantity']
+    # # Get orders with promo code
+    # orders = g.db['test2']['new_filled_orders']
+    # orders = orders.find({'promo_code': promo_code, 'financial_status': 'paid'}, {'lineitem': 1})
+    # for order in orders:
+    #     #find corresponding product
+    #     product_name = order['lineitem'][0]['lineitem_name']
+    #     result = next((item for item in products if item['title'] == product_name), None)
+    #     price = order['lineitem'][0]['lineitem_price']
+    #     quantity = order['lineitem'][0]['lineitem_quantity']
 
-        #update attributes
-        result['total_products_purchased'] += quantity
+    #     #update attributes
+    #     result['total_products_purchased'] += quantity
 
-        create_time = order['lineitem'][0]['created_at']
-        # 看是否在带货列表中且购买日期在签约日期间
-        create_time  = datetime.strptime(create_time , '%Y-%m-%d %H:%M:%S %z').strftime('%Y-%m-%d')
-        create_time = datetime.strptime(create_time , '%Y-%m-%d')
+    #     create_time = order['lineitem'][0]['created_at']
+    #     # 看是否在带货列表中且购买日期在签约日期间
+    #     create_time  = datetime.strptime(create_time , '%Y-%m-%d %H:%M:%S %z').strftime('%Y-%m-%d')
+    #     create_time = datetime.strptime(create_time , '%Y-%m-%d')
 
-        if result['status'] and create_time > datetime.strptime(result['start_time'], '%m-%d-%Y') and create_time < datetime.strptime(result['end_time'], '%m-%d-%Y'):
-            #很奇怪，price是从order里获取的。如果同一个商品的不同order的不同price那怎么办
-            result['earnings_per_product'] = float(result['commission_rate'].replace('%', ''))/100 * price
-        else: 
-            result['earnings_per_product'] =  price * 0.08
-        result['total_commission'] += result['earnings_per_product'] * quantity
+    #     if result['status'] and create_time > datetime.strptime(result['start_time'], '%m-%d-%Y') and create_time < datetime.strptime(result['end_time'], '%m-%d-%Y'):
+    #         #很奇怪，price是从order里获取的。如果同一个商品的不同order的不同price那怎么办
+    #         result['earnings_per_product'] = float(result['commission_rate'].replace('%', ''))/100 * price
+    #     else: 
+    #         result['earnings_per_product'] =  price * 0.08
+    #     result['total_commission'] += result['earnings_per_product'] * quantity
 
     return products
